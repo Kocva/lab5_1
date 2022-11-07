d2df5dbf5cd0ccbd3e5f8ad5385930b763bbbc2a
Kocva <Kocva2003@mail.ru>
2022-11-07 12:53:35 +0800

    ADD output of objects

--- lab5_1/Form1.cs
+++ lab5_1/Form1.cs
@@ -1,10 +1,32 @@
+using lab5_1.Objects;
+
 namespace lab5_1
 {
     public partial class Form1 : Form
     {
+        MyRectangle myRect;
+        List<BaseObject> objects = new();
+
         public Form1()
         {
             InitializeComponent();
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(Color.White);
+
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+            //g.Transform = myRect.GetTransform();
+            
+            //myRect.Render(g);
         }
     }
 }
